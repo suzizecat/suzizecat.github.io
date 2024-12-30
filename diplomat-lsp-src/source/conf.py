240bf02eab0b7221cd8723a2c9a792ae92a9147b
Julien FAUCHER <suzizecat@free.fr>
2024-12-31 00:28:17 +0100

    Add infos on scoped completion

--- diplomat-lsp-src/source/conf.py
+++ diplomat-lsp-src/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Julien FAUCHER'
 author = 'Julien FAUCHER'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
